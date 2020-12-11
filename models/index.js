6d5dd4e01a9277ee2c976ee561e9dc4891b47f38
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2020-12-10 18:37:59 -0700

    belongsTo and hasMany started

--- models/index.js
+++ models/index.js
@@ -5,8 +5,17 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: '',
+  onDelete: 'CASCADE',
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+foreignKey: '',
+onDelete: 'CASCADE',
+});
+
 
 // Products belongToMany Tags (through ProductTag)
 
