8f5a1a56efe39928e09e244ad17f003c81a2fa1e
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2020-12-11 14:43:18 -0700

    Get method routes created for tag-routes.js

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -5,12 +5,28 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 router.get('/', (req, res) => {
   // find all tags
-  // be sure to include its associated Product data
+  try {
+    const tagData = await Tag.findAll({
+      // be sure to include its associated Product data
+      include: { model: Product },
+    });
+    res.catch(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
-  // be sure to include its associated Product data
+  try {
+    const tagData = await Tag.findByPk({
+      // be sure to include its associated Product data
+      include: { model: Product },
+    });
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.post('/', (req, res) => {
